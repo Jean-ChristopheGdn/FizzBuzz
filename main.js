56a7eb9dcc9095288a4bc787f9eb779199dac57a
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-20 09:47:06 +0100

    [add] fizzbuzz

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-document.getElementById("result");
+let result = document.getElementById("result");
+let getNumber = document.getElementById("number");
 
-function fizzBuzz(){
-    
+function fizzBuzz() {
+    let number = getNumber.value;
+    for (let i = 0; i <= number; i++) {
+        if (i == 0) {
+            result.innerHTML += i + "<br>";
+        }
+        else if (i % 3 == 0 && i % 5 == 0) {
+            result.innerHTML += i + ": FizzBuzz<br>";
+        }
+        else if (i % 3 == 0) {
+            result.innerHTML += i + ": Fizz<br>";
+        } 
+        else if (i % 5 == 0) {
+            result.innerHTML += i + ": Buzz<br>";
+        } 
+        else {
+            result.innerHTML += i + "<br>";
+        }
+    }
 }
